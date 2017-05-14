d7bf306d7a19bd796fec910ae9adb76f70a3d1de
Jason Yu <me@ycmjason.com>
2017-05-14 02:22:11 +0100

    added minified app

--- app/services/stories.service.js
+++ app/services/stories.service.js
@@ -2,5 +2,5 @@ import { stories } from './stories.data';
 
 export var storiesService = [function(){
   // Promise is used so that later one could easily change this service to call asynchronously to a server to fetch stories
-  this.getStories = () => Promise.resolve(stories);
+  this.getStories = () => Promise.resolve(JSON.parse(JSON.stringify(stories)));
 }];
