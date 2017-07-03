8b5d1676a44e66e9ef17e9473df6f9d1de5e3019
zcjtshy <see.yim.13@ucl.ac.uk>
2017-07-03 08:12:39 +0100

    added otherstories.data

---
+++ app/services/otherstories.data.js
@@ -0,0 +1,87 @@
+{
+      id:1,
+      title:'',
+      name:'Museum of Homelessness',
+      image:'',
+      filter:['',''],
+      story: `Exchanged presents stories you won’t hear anywhere else. Part installation and part story-telling space, Exchanged is a display of objects from around the UK that offers a snapshot of what is happening in detention centres, in squats, in alternative communities, in hostels, day centres and on the streets, objects have been donated from a wide range of sources including homelessness workers, volunteers, activists and people experiencing various forms of homelessness.The objects were grouped into four zones that show responses to homelessness – compassion, cruelty, resistance and process. We have the objects in zones related to these words for the show and we invite our audience to choose where they would place them, once they have heard the testimony. If you have a story to share inspired by these objects, you can do that too by speaking to one of our team.`,
+},
+{
+		id:2,
+      title:'',
+      name:'Museum of Transology',
+      image:'',
+      filter:['',''],
+      story: `Due to transphobia, when trans people come out they often lose contact with communities from their earlier lives, including family, school and work friends, past partners and children. Coupled with the fact that they are unlikely to have grown up surrounded by trans people, the reality for many is a social displacement that occurs because of a dissociation from one’s personal theirstory. This displacement can also be physical, with one in three survey respondents to the Trans Needs Assessment 2015 reporting that they had been homeless in the last five years. This obstracism can be fought not only by the strength of supportive trans communities, but also through the presentation of transcestry in historical institutions. This is precisely what museums should do – offer engagement with the past as a way of making sense of the now, in order to inform a positive and responsible future. In the words of LGBT historian Susan Fernetinos, ‘To offer roots to those who have at one time or another found themselves without any is a very powerful gift indeed.’ The selection of objects represents milestones of their owners’ gender journeys`,
+},
+{
+	id:3,
+      title:'The Face of Migration. 15 Women with Roots in Greece tell their Stories',
+      name:'Nordic Museum',
+      image:'',
+      filter:['',''],
+      story:`Objects are carriers of stories. They can evoke memories of fragrances, tastes, sounds and light. What objects that become important cultural carriers when leaving your country for another is very individual. We asked 15 members of the Lyceum Club of Greek Women, Stockholm. In the exhibition you will meet them through their stories and chosen objects. Today there are about 20 000 people of Greek origin living in Sweden. Most of them came during the years 1965-1970. They came to Sweden for different reasons: through marriage, to study, to work or to seek protection for political views.
+      The Sunday before the wedding my grandmother’s dowry was put into the chest. Born in Pireus, came to Sweden in 1982 to work at the embassy, resident since 2007. The dress and the chest has belonged to my grandmother, Simerouda Gioka (1886-1965) born on the island Salamis. She was married to my grandfather in 1911. He was a sea captain. When I visited my grandmother as a child I used to sit on the chest. I in my turn received the chest as a wedding gift and when I opened it I found my grandmothers dress in it. To me these objects are two holy relics.`,
+},
+{
+	id:4,
+	title:'',
+	name:'Objects of Desire',
+	image:'',
+	filter:[''],
+	story:`Exploring sex work through object biographies, Objects of Desire provides an alternate narrative of sex work to the binary ‘exploitation versus empowerment’ discourse that dominates popular discourse around sex work.Objects of Desire (OoD) is a collaborative project led by sex workers and artists. We aim to preserve sex workers’ stories through archiving and exhibiting the artefacts of sex workers. We invite current and former sex workers to submit photographs, objects (if possible) and narratives. Objects might include gifts received from clients, good luck charms or sex work activism memorabilia. Through focusing on the connections between personal narratives and material objects, OoD purposefully shifts the conversation on sex work from one that focuses on the objectification of people to an examination of the social relations seen through the lens of physical things. It examines how objects both reflect and shape the everyday relations sex workers have with their clients, lovers, families and others.The project aims to challenge the one-dimensional stereotypes that dominate popular discourse on sex work. While sex worker organisations worldwide continue to fight for decriminalisation as the best method to secure sex workers’ rights and better working conditions, debates surrounding legislation still repeatedly exclude sex workers’ voices.bjects of Desire takes the practice and everyday experience of sex work as a way of disrupting binaries that categorise sex workers as either too privileged or too victimised to exercise their own agency. Through foregrounding sex workers’ voices and stories and creating a living record of their artefacts, OoD challenges the pervasive social stigma that attempts to silence sex workers. It is not merely an attempt to ‘humanize’ sex workers through providing outsiders with a glimpse into their lives. Rather, sex workers’ stories about materiality and exchange challenge the wider public to reflect upon the dynamics of gendered labor, complex hierarchies of power and care under capitalism, as well as the interplay of the emotional and the material in all relationships`,
+},
+{
+	id:5,
+	title:'A poem',
+	name:'Hikayetna. Stories from Syria, Hope and Optimism for Refugees',
+	image:'',
+	filter:[''],
+	story:`I left that table with three books, a tea glass dirty
+An ashtray
+The TV remote still lost somewhere between cushions
+A wall with a mixture of rotten green broken yellow light
+Small window into an empty street
+A white tissue travels lonely in a windy ruined alley
+I left a pregnant apple tree
+A sink full of pans, has remnant of favourite dish from last night
+My plate among them with a tulip
+I left half a bottle of red wine near bed
+Money notes wrinkled
+A belt with broken buckle
+The art work in the corridor
+The man in it hand on cheek tearful eyes
+The forest behind him huge as the memory it leaves behind
+I left a tape player once a lover gifted me
+The Kurdish singer Mohammed Sixo on it screams
+Oh the land Oh the land
+I left my school desk with my name engraved,
+The teacher who lectures me every time I bring a poetry book
+Instead of syllabus book
+I left the old corner shop
+With a debt book
+That has my name
+Left the new shoe yet to wear
+The yellow laces I bought
+To go with it
+My mother who stops by the door signals “come food is ready”
+I left a generous father who daily comes home with bags of figs, apples
+And occasionally roast chicken in right hand
+I left home.`,
+},
+{
+	id:6,
+	title:'Gourd, Earth and Water',
+	name:'British or Chinese? Stories of Migration, Family and Identity by SOAS',
+	image:'',
+	filter:[''],
+	story:`My mother gave me a pouch of earth from our garden in Taipei, a tiny bottle of water and a gourd. She told me to pour the water into the well of my new home and sprinkle the earth around my garden. That way I wouldn’t have any problems acclimatizing to my new environment. The gourd was meant to protect me from illnesses.`,
+},
+{
+	id:7,
+	title:'Chinese checkers, draughts and live-in-a-row',
+	name:'British or Chinese? Stories of Migration, Family and Identity by SOAS',
+	image:'',
+	filter:[''],
+	story:`I bought these games that I always used to play as a child with my grandmother in Shanghai. She would often play mahjong. I can remember so clearly the sound of the tiles being shuffled around on the table. When I was alone I would just kick the shuttlecock and see how long I could keep it going.`,
+} 
